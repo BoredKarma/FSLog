cffd526ef8f1aad1eff53a99fbed15d382ab978b
Karma <106445107+BoredKarma@users.noreply.github.com>
2024-04-24 19:53:55 +0000

    project structure and build system

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+COMPILER="g++"
+FLAGS="-std=c++14 -pthread -fexceptions -Werror -Wextra -Wpedantic -Wshadow -Wconversion -Wnull-dereference"
+INCLUDE_DIR="include"
+
+SOURCE="main.cpp"
+OUT_DIR="build"
+EXECUTABLE="fslog"
+
+mkdir -p $OUT_DIR
+
+$COMPILER $FLAGS $SOURCE -o $OUT_DIR/$EXECUTABLE -I$INCLUDE_DIR
+
+echo "Build complete."
