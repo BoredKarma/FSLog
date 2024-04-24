fcae500da62d8bdf8d80c99930e782c4aac76476
Karma <106445107+BoredKarma@users.noreply.github.com>
2024-04-24 12:56:26 -0700

    build.bat

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set COMPILER=g++
+set FLAGS=-std=c++14 -fexceptions -Werror -Wextra -Wpedantic -Wshadow -Wconversion -Wnull-dereference
+set SOURCE=main.cpp
+set INCLUDE_DIR=include
+
+set OUT_DIR=build
+set EXECUTABLE=fslog
+
+if not exist %OUT_DIR% mkdir %OUT_DIR%
+
+%COMPILER% %FLAGS% %SOURCE% -o %OUT_DIR%/%EXECUTABLE% -I %INCLUDE_DIR%
+
+echo Build complete.
